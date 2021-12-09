d7990f356666801ffceaff7635406f1dd9643b3c
Mateusz Woda <mwoda@box.com>
2021-12-09 12:47:14 +0100

    Update bump_version.ps1

--- build/bump_version.ps1
+++ build/bump_version.ps1
@@ -44,7 +44,7 @@ if ($InstallDependencies){
 
 Invoke-Expression "& `"$GIT_SCRIPT`" -b $Branch"
 if ($LASTEXITCODE -ne 0) {
-   #exit 1
+   exit 1
 }
 
 ###########################################################################
@@ -73,10 +73,7 @@ $RELEASE_NOTE_LINK = $NEXT_VERSION.Replace(".", "") + "-" + "$RELEASE_DATE"
 
 if($DryRun){
     Write-Output "Dry run. PR with version bump will not be created."
-}else{
-    git remote set-url origin https://mwwoda:$GithubToken@github.com/mwwoda/net-sdk-playground.git
-    git config user.mail "mwoda@box"
-    git config user.name "mwwoda" 
+}else{ d
     git branch -D $NEXT_VERSION_TAG
     git checkout -b $NEXT_VERSION_TAG
     git commit -am $NEXT_VERSION_TAG
