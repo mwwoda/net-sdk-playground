72395dfc5b627d101b658d6687c3a6d7f5237344
mwwoda <mwoda@box.com>
2022-02-09 06:28:58 -0800

    chore: release v1.15.0

--- Net.Sdk.Playground/Utility/AssemblyInfo.cs
+++ Net.Sdk.Playground/Utility/AssemblyInfo.cs
@@ -5,7 +5,7 @@ namespace Net.Sdk.Playground.Utility
         public const string AssemblyTitle = "Net.Sdk.Playground";
         public const string AssemblyProduct = "Net.Sdk.Playground";
 
-        public const string NuGetVersion = "1.14.0";
+        public const string NuGetVersion = "1.15.0";
         public const string AssemblyVersion = "1.0.0.0";
         public const string AssemblyFileVersion = "1.0.0.0";
     }
