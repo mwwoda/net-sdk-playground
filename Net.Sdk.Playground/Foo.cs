a8c13ac6058fd615826f8d9031ee099f1a7ef8fa
mwoda <mwoda@box.com>
2022-11-24 13:57:09 +0100

    feat: new feature

--- Net.Sdk.Playground/Foo.cs
+++ Net.Sdk.Playground/Foo.cs
@@ -10,7 +10,7 @@ namespace Net.Sdk.Playground
     {
         public string Bar()
         {
-            var a = "bbbbbbbbbbbbbb";
+            var a = "bbbbbbbbbbbbbbb";
             return "Bar";
         }
 
