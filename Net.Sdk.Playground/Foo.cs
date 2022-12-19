61ba537a325ed446b9cb5733bb6e0ad5dfaac386
mwoda <mwoda@box.com>
2022-12-19 09:51:57 +0100

    feat!: breaking change

--- Net.Sdk.Playground/Foo.cs
+++ Net.Sdk.Playground/Foo.cs
@@ -10,7 +10,7 @@ namespace Net.Sdk.Playground
     {
         public string Bar()
         {
-            var a = "bbbbbbbbbbbbbbb";
+            var a = "bbbbbbbbbbbbbbbb";
             return "Bar";
         }
 
