cbd5058de13943f56719742047fa3f9d0f73371d
mwoda <mwoda@box.com>
2023-02-22 17:36:36 +0100

    chore: cross ref box/box-openapi@2fdb5b4 branch

--- Net.Sdk.Playground/Foo.cs
+++ Net.Sdk.Playground/Foo.cs
@@ -10,7 +10,7 @@ namespace Net.Sdk.Playground
     {
         public string Bar()
         {
-            var a = "bbbbbbbbbbbbbbbbbbbb";
+            var a = "bbbbbbbbbbbbbbbbbbbbb";
             return "Bar";
         }
 
