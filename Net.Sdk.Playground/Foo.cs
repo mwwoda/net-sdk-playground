14163ba466a8ea5af3caabfe737a074852a38b6f
mwoda <mwoda@box.com>
2023-02-22 11:52:25 +0100

    chore: ref cbe8820 test

--- Net.Sdk.Playground/Foo.cs
+++ Net.Sdk.Playground/Foo.cs
@@ -10,7 +10,7 @@ namespace Net.Sdk.Playground
     {
         public string Bar()
         {
-            var a = "bbbbbbbbbbbbbbbbbb";
+            var a = "bbbbbbbbbbbbbbbbbbb";
             return "Bar";
         }
 
